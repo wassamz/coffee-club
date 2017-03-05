443d2353ff8324aa57f8a8cc650041cd1ce571f2
Wassam Zahreddine <Wassam.Zahreddine@scotiabank.com>
2017-03-04 23:41:03 -0500

    Removed this unused semantic ui module that didn't play nice with @Angular/cli

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -2,7 +2,6 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
-import { SuiModule } from 'ng2-semantic-ui/ng2-semantic-ui';
 
 import { AppComponent} from './app.component';
 import { ListItemPersonComponent } from './list-item-person/list-item-person.component';
@@ -19,8 +18,7 @@ import { PairMembersComponent } from './pair-members/pair-members.component';
     BrowserModule,
     FormsModule,
     ReactiveFormsModule,
-    HttpModule,
-    SuiModule
+    HttpModule
   ],
   providers: [PeopleService],
   bootstrap: [AppComponent]
